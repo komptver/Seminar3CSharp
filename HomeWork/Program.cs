3830ca4d5e2b5ee659e734f6f31a5d89d06724aa
Aleksandr <komptver@gmail.com>
2022-12-19 10:08:20 +0300

    decided second task

---
+++ HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double DinstanceAB(double xa, double ya, double za, double xb, double yb, double zb)
+{
+double length  = Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya,2) + Math.Pow(zb-za,2)); //Расстояние» d = √ [(x2-x1)2+ (y2-y1)2],
+return Math.Round(length, 2);
+}
+
+Console.WriteLine("Input an x-coordinate of point a: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input an y-coordinate of point a: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input an z-coordinate of point a: ");
+double za = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input an x-coordinate of point b: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input an y-coordinate of point b: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input an z-coordinate of point b: ");
+double zb = Convert.ToDouble(Console.ReadLine());
+double distance = DinstanceAB(xa,ya,za,xb,yb,zb);
+Console.WriteLine($"distance between three points a and b is {distance}");
